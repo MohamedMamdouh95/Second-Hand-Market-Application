61f0aa35c97ead4692554f309bada6b3c4e4ecf0
MohamedMamdouh95 <mohamedmtourab@gmail.com>
2020-04-02 17:45:47 +0300

    Fixed the context menu bug

--- app/src/main/java/com/example/second_hand_market_application/EditProfileActivity.kt
+++ app/src/main/java/com/example/second_hand_market_application/EditProfileActivity.kt
@@ -2,22 +2,24 @@ package com.example.second_hand_market_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.View
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
-//    private lateinit var imageButton:ImageButton
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_edit_profile)
-//        imageButton = findViewById(R.id.imageButton1)
-//        registerForContextMenu(imageButton)
-//    }
-//    override fun onCreateContextMenu(menu: ContextMenu, v: View,
-//                                     menuInfo: ContextMenu.ContextMenuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo)
-//        val inflater: MenuInflater = menuInflater
-//        inflater.inflate(R.menu.change_picture, menu)
-//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+        registerForContextMenu(profileImageButton)
+    }
+    override fun onCreateContextMenu(menu: ContextMenu, v: View,
+                                     menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.change_picture, menu)
+    }
 
 
 }
--- app/src/main/java/com/example/second_hand_market_application/ShowProfileActivity.kt
+++ app/src/main/java/com/example/second_hand_market_application/ShowProfileActivity.kt
@@ -1,19 +1,21 @@
 package com.example.second_hand_market_application
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_show_profile.*
 
 class ShowProfileActivity : AppCompatActivity() {
-    private lateinit var imageButton: ImageButton
+    companion object {
+        const val EDIT_PROFILE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        imageButton = findViewById(R.id.imageButton2)
-        registerForContextMenu(imageButton)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -21,16 +23,39 @@ class ShowProfileActivity : AppCompatActivity() {
         inflater.inflate(R.menu.edit_menu, menu)
         return true
     }
-    override fun onCreateContextMenu(menu: ContextMenu, v: View,
-                                     menuInfo: ContextMenu.ContextMenuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        Log.d("test","you clicked the Image button")
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.change_picture, menu)
-    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d("test","you clicked the button")
-        return true
+        return when (item.itemId) {
+            R.id.pencil -> {
+                editProfile()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
+
+    private fun editProfile() {
+        val detailIntent = Intent(this, EditProfileActivity::class.java)
+        detailIntent.putExtra("fullName", fullName.text.toString())
+        detailIntent.putExtra("nickName", nickname.text.toString())
+        detailIntent.putExtra("location", location.text.toString())
+        detailIntent.putExtra("email", email.text.toString())
+        val imgURI: String = if (profileImage.tag == "default") {
+            Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + resources.getResourcePackageName(
+                    R.drawable.profile_pic
+                ) + '/' + resources.getResourceTypeName(R.drawable.profile_pic) + '/' + resources.getResourceEntryName(
+                    R.drawable.profile_pic
+                )
+            ).toString()
+        } else {
+            profileImage.tag.toString()
+        }
+        detailIntent.putExtra("imageURI", imgURI)
+
+        startActivityForResult(detailIntent, EDIT_PROFILE)
+    }
+
 }
+
