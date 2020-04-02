ec46701ff7fe961a0f26e352d456dcfef47b7389
MohamedMamdouh95 <mohamedmtourab@gmail.com>
2020-04-02 18:16:59 +0300

    used onRestoreInstanceState instate of using savedInstanceState:Bundle variable inside onCreate to recover the data

--- app/src/main/java/com/example/second_hand_market_application/ShowProfileActivity.kt
+++ app/src/main/java/com/example/second_hand_market_application/ShowProfileActivity.kt
@@ -17,14 +17,15 @@ class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        if (savedInstanceState != null) {
-            fullName.text = savedInstanceState.get("fullName").toString()
-            nickname.text = savedInstanceState.get("nickName").toString()
-            location.text = savedInstanceState.get("location").toString()
-            email.text = savedInstanceState.get("email").toString()
-        }
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        fullName.text = savedInstanceState.get("fullName").toString()
+        nickname.text = savedInstanceState.get("nickName").toString()
+        location.text = savedInstanceState.get("location").toString()
+        email.text = savedInstanceState.get("email").toString()
+    }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("fullName", fullName.text.toString())
